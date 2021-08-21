00da9385ea8a81ce8b030d9b8ff655ab607a1ce2
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2021-08-21 20:49:22 +0430

    config: bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import './assets/sass/app.scss'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,4 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
+reportWebVitals()
