ac6c2ee13b4ed45bb020c00372017fcc260f2936
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2021-08-28 21:18:56 +0430

    feat: about page ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import HomePage from './pages/Home'
 import PostPage from './pages/Post'
+import AboutPage from './pages/About'
 import Header from './components/Layout/Header'
 import Footer from './components/Layout/Footer'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -9,8 +10,9 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Route exact path="/" component={ HomePage } />
-        <Route exact path="/post/:id/:slug" component={ PostPage } />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/post/:id/:slug" component={PostPage} />
+        <Route exact path="/about" component={AboutPage} />
         <Footer />
       </Router>
     </div>
